03e151771120cef0aec969edbc2ff953623b5f82
Fantin <fantin@malou.io>
2023-05-11 16:56:21 +0200

    refresh for env vars

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export async function handler(event: {
     Records: { s3: S3Event }[];
 }): Promise<any> {
     try {
+        //
         const s3Event = event.Records[0].s3
         console.info("[START_S3_EVENT] : ", s3Event)
         const converter = new ConvertService(s3Event);
