89e51d8950fa5f7ff580bffa3b1ba7f37f421a8d
Fantin <fantin@malou.io>
2023-05-11 14:40:22 +0200

    setup lambda

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+export async function handler(event: any): Promise<any> {
+    try {
+        console.info("[START] : ", event);
+    } catch (e) {
+        console.error("[ERROR] : ", e);
+    } 
+    return 0;
+}
