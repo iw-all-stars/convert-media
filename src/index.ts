26c7e37fe30cb21ed0d1f1b9dc96f6586b534f8a
Fantin <fantin@malou.io>
2023-05-11 14:51:50 +0200

    log

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 ////
 export async function handler(event: any): Promise<any> {
     try {
-        console.info("[START] : ", event);
+        console.info("[START] : ", event.Records[0].s3);
+        console.info("[LOG2] : ", event.Records[0].s3.object);
+        console.info("[KEY] : ", event.Records[0].s3.object.key);
     } catch (e) {
         console.error("[ERROR] : ", e);
     } 
